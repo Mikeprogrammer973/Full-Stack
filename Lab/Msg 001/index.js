b65745ec8a4b3e661dc054e14d3d124083b227e1
Mike_Sognatore <mikepascal.delta@gmail.com>
2023-07-16 16:59:29 -0400

    Msg update!

--- Lab/Msg 001/index.js
+++ Lab/Msg 001/index.js
@@ -1,4 +1,5 @@
 // Método 1
+/*
 var myHeaders = new Headers();
 myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
 
@@ -19,19 +20,37 @@ fetch("https://api.ultramsg.com/instance53575/messages/chat", requestOptions)
   .then(response => response.text())
   .then(result => console.log(result))
   .catch(error => console.log('error', error));
-
+*/
 
 // Método 2
 const accountSid = 'ACe5332a0668b418772a6937ee7c579891';
 const authToken = '0b574194176cd7366cedf1ef7f8ea062';
 const client = require('twilio')(accountSid, authToken);
 
-for(let i = 0; i < 10; i++){
-    client.messages
+function send_msg(msg, to){
+  client.messages
     .create({
-        body: 'Your appointment is coming up on July 21 at 3PM',
+        body: msg,
+        mediaUrl: ['https://live-s3-bucket-sjwburhj9xhf.cdn.live.tandem.net/e2/19/90b1181cd60ae0688ff15047894eb337.jpg'],
         from: 'whatsapp:+14155238886',
-        to: 'whatsapp:+5216864631922'
+        to: `whatsapp:${to}`
     })
     .then(message => console.log(message.sid))
 }
+
+const fs = require('fs')
+const readline = require('readline')
+
+async function get_nums(file) {
+  const fileStream = fs.createReadStream(file);
+  const rl = readline.createInterface({
+    input: fileStream,
+    crlfDelay: Infinity
+  })
+  for await (const line of rl) {
+    console.log(line)
+    send_msg("We're going on!", line)
+  }
+}
+ 
+get_nums('./Lab/Msg 001/nums.txt')
