e41380d17c933fa414ab66d87b502a6d3fde9a7f
Mike_Sognatore <mikepascal.delta@gmail.com>
2023-08-14 18:43:01 -0400

    Bolas flutuentes done!

--- Javascript/Curso 002/BOLAS/index.js
+++ Javascript/Curso 002/BOLAS/index.js
@@ -19,17 +19,21 @@ class Bola
         this.color = `rgb(${Math.round(Math.random()*255)}, ${Math.round(Math.random()*255)}, ${Math.round(Math.random()*255)})`
         this.pX = Math.round(Math.random()*(width_tela - this.size))
         this.pY = Math.round(Math.random()*(height_tela - this.size))
-        this.velX = this.velY = Math.round(Math.random()*3) + 0.5
+        this.velX = Math.round(Math.random()*3) + 1.5
+        this.velY = Math.round(Math.random()*3) + 0.9
         this.dirX = this.dirY = (Math.random()*10) > 5 ? 1 : -1
         this.tela = tela
         this.id = Date.now() + "_" + Math.round(Math.random()*1000000000000)
         this.draw()
-        this.ctrl = setInterval(this.control, 10)
         this.bola = document.getElementById(this.id)
+        this.ctrl = setInterval(()=>{
+            this.control(this.bola)
+        }, 10)
     }
 
     draw(){
         const dv = document.createElement("div")
+        dv.id = this.id
         dv.style.width = `${this.size}px`
         dv.style.height = `${this.size}px`
         dv.style.backgroundColor = this.color
@@ -47,8 +51,35 @@ class Bola
         })
     }
 
-    control(){
+    remover(pos){
+        bolas.splice(pos, 1)
+        this.bola.remove()
+    }
 
+    verify_limit_event(pos){
+        if(this.pX + this. size > width_tela){
+            this.remover(pos)
+        }
+    }
+
+    control(){
+        // Horizontal
+        if(this.pX + this. size >= width_tela){
+            this.dirX = -1
+        }else if(this.pX + this.size <= 0){
+            this.dirX = 1
+        }
+        // Vertical
+        if(this.pY + this. size >= height_tela){
+            this.dirY = -1
+        }else if(this.pY + this.size <= 0){
+            this.dirY = 1
+        }
+
+        this.pX += this.velX * this.dirX
+        this.pY += this.velY * this.dirY
+        this.bola.style.left = `${this.pX}px`
+        this.bola.style.top = `${this.pY}px`
     }
 }
 
@@ -63,8 +94,15 @@ window.onload = ()=>{
     height_tela = tela.offsetHeight
 
     window.addEventListener("resize", ()=>{
+        // Atualizar altura e largura da tela
         width_tela = tela.offsetWidth
         height_tela = tela.offsetHeight
+
+        bolas.map((b, i)=>{
+            b.verify_limit_event(i)
+            inpt_tot_objs.value = bolas.length
+            //console.log(bolas)
+        })
     })
 
     btn_add_objs.addEventListener("click", ()=>{
@@ -77,12 +115,20 @@ window.onload = ()=>{
         }
         inpt_add_objs.value = ""
         inpt_tot_objs.value = bolas.length
-        console.log(bolas)
+        //console.log(bolas)
+
+        bolas.map((b, i)=>{
+            b.bola.addEventListener('click', ()=>{
+                b.remover(i)
+                inpt_tot_objs.value = bolas.length
+                //console.log(bolas)
+            })
+        })
     })
 
     btn_remove_all.addEventListener("click", ()=>{
         Bola.remover_all()
         inpt_tot_objs.value = bolas.length
-        console.log(bolas)
+        //console.log(bolas)
     })
 }
