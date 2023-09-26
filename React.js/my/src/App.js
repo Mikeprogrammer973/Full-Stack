f76fad6175e28cc8c72d69240366181cb84de967
Mike_Sognatore <mikepascal.delta@gmail.com>
2023-09-26 19:22:34 -0400

    Manipulando elementos de formulário com React

--- React.js/my/src/App.js
+++ React.js/my/src/App.js
@@ -1,26 +1,24 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 import components from './components/components';
+import { useState } from 'react';
 
 function App() {
+
+  const [nome, setNome] = useState('')
+
   return (
     <>
       <components.Btn text={"Start"}/>
       <components.Horloge/>
       <hr></hr>
+
       <components.Container>
-        <components.Task name={"Learn Node.js"} isDone={true}/>
-        <components.Task name={"Learn JavaScript"} isDone={true}/>
-        <components.Task name={"Learn TypeScript"} isDone={false}/>
-        <components.Task name={"Learn React"} isDone={false}/>
-        <components.Task name={"Learn C#"} isDone={true}/>
-        <components.Task name={"Learn Saass"} isDone={false}/>
-      </components.Container>
-      <components.Container>
-        <components.Gallery/>
-      </components.Container>
-      <components.Container>
-        <components.Counter/>
+        <form>
+          <input 
+            onChange={(e)=>{setNome(e.target.value)}} value={nome} className='form-control' type='text' placeholder='seu nome...' name='ipt-nome'></input>
+          <p>Nome digitado: {nome}</p>
+        </form>
       </components.Container>
     </>
   );
