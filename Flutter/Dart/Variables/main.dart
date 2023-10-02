4939386d2b3f7a9fb8ecb17354c08e0f9db10fb0
MikePascal <mikepascal.delta@gmail.com>
2023-10-02 19:00:50 -0400

    Tipos de Variáveis no Dart

---
+++ Flutter/Dart/Variables/main.dart
@@ -0,0 +1,24 @@
+void main() {
+  // Produtos
+
+  int codigo = 7;
+
+  double preco = 2.85;
+
+  String nome = 'Sabonete Iris';
+
+  bool desconto = true;
+
+  var loja = 'My Stores';
+  //loja = 4;
+
+  dynamic variable = 74;
+  variable = '00K';
+
+  print(codigo);
+  print(preco);
+  print(nome);
+  print(desconto ? 'Oui' : 'Non');
+  print(loja);
+  print(variable);
+}
