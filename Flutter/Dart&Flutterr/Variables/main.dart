dd3d293556221f40054fea80aff45743fa3326b0
MikePascal <mikepascal.delta@gmail.com>
2023-10-04 07:49:35 -0400

    Switch Case e Break em Dart

--- Flutter/Dart&Flutterr/Variables/main.dart
+++ Flutter/Dart&Flutterr/Variables/main.dart
@@ -27,11 +27,36 @@ void main() {
 
   //print('Code: ' + codigo.toString());
 
-  print('O produto $codigo é um $nome e o seu preço é $preco reais.');
+  print(
+      'O produto $codigo é um $nome e o seu preço é $preco reais.\nEn promotion: ${(desconto ? 'Oui' : 'Non')}');
 
   print(check_num(7));
   print(check_num(456));
   print(check_num(251));
+
+  if (0 > 9 && 7 > 9) {
+    print('OK 1');
+  } else {
+    print('NO OK 1');
+    if (9 >= 21 || 0 == 0) {
+      print('OK 2');
+    }
+  }
+
+  int num = 3;
+
+  switch (num) {
+    case 1:
+    case 2:
+    case 3:
+      print('--$num--');
+      break;
+    case 7:
+      print('Jackpot!!!');
+      break;
+    default:
+      print('__ $num __');
+  }
 }
 
 String check_num(int num) {
