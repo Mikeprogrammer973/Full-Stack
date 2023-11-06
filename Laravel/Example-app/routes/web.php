4abb9fc7aa9f1eab7452929779c1f8727d10a14b
Mike_Sognatore <mikepascal.delta@gmail.com>
2023-11-06 10:34:29 -0500

    Rutas & Controladores en Laravel

--- Laravel/Example-app/routes/web.php
+++ Laravel/Example-app/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\CursoController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', HomeController::class);
+
+Route::get("cursos", [CursoController::class, "index"]);
+
+Route::get("cursos/create", [CursoController::class, "create"]);
+
+Route::get("cursos/{curso}", [CursoController::class, "show"]);
+
+/*Route::get("cursos/{curso}/{categoria?}", function($curso, $cat = null){
+    return "Bienvenido al curso $curso".($cat != null ? ", de la categoria $cat!": "!");
+});*/
+
