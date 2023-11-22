46c6a58d608ccd61e0fa05e446159ebba7feaeaa
Mike_Sognatore <mikepascal.delta@gmail.com>
2023-11-22 12:03:14 -0500

    Cómo listar y leer registros en Laravel

--- Laravel/Example-app/resources/views/cursos/index.blade.php
+++ Laravel/Example-app/resources/views/cursos/index.blade.php
@@ -5,4 +5,14 @@
 
 @section('content')
     <h1>Bienvenido a la pagina de cursos!</h1>
+    <a href={{ route('cursos.create') }}>Crear Curso</a>
+    <ul>
+        @foreach ($cursos as $curso)
+            <li> 
+                <a href={{ route('cursos.show', $curso->id) }}>{{$curso->name}}</a>
+            </li>
+        @endforeach
+    </ul>
+
+    {{$cursos->links()}}
 @endsection()
