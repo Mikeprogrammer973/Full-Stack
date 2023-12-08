ec470d7f7c533528e3593700e6150e4e96a187ba
Mike_Sognatore <mikepascal.delta@gmail.com>
2023-12-08 14:48:06 -0500

    La grid de Tailwind

--- Tailwind CSS/tailwind/resources/views/welcome.blade.php
+++ Tailwind CSS/tailwind/resources/views/welcome.blade.php
@@ -9,8 +9,26 @@
     <link rel="stylesheet" href={{ asset('css/app.css') }}>
 </head>
 <body>
-    <h1 class="text-3xl font-bold underline">
-        Hello world!
-    </h1>
+    <div class="container">
+        <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2"><!-- gap-x-2 / gap-y-2 -->
+            <div class="bg-blue-200">A</div><!--  col-span-2 col-start-2 md:col-span-3 lg:col-span-4 -->
+            <div class="bg-blue-400">B</div>
+            <div class="bg-blue-600 lg:col-span-2 row-span-2">C</div>
+            <div class="bg-blue-800">D</div>
+            <div class="bg-blue-100">E</div>
+        </div>
+        <hr>
+        <div class="grid grid-flow-col grid-rows-3">
+            <div class="bg-orange-100">1</div>
+            <div class="bg-orange-200">2</div>
+            <div class="bg-orange-300">3</div>
+            <div class="bg-orange-400">4</div>
+            <div class="bg-orange-500">5</div>
+            <div class="bg-orange-600">6</div>
+            <div class="bg-orange-700">7</div>
+            <div class="bg-orange-800">8</div>
+            <div class="bg-orange-900">9</div>
+        </div>
+    </div>
 </body>
 </html>
