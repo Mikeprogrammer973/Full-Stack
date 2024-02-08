98ce742cc6047d196f50eb4da699bf97038874cb
Mike_Sognatore <mikepascal.delta@gmail.com>
2024-02-07 19:27:48 -0500

    Python learning started...

---
+++ Python/Hello/hello.py
@@ -0,0 +1,10 @@
+
+# Desafio 001
+
+nome = input("Qual é o seu nome? ")
+print("\n\nOlá, " + nome + ", prazer em te conhecer!\n")
+
+# Desafio 002
+
+print("\n\n {}, você nasceu no dia ".format(nome) + input("Dia: ") + " de " + input("Mês: ") + " de " + input("Ano: ") + ", correto?\n\n")
+
---
+++ Python/Ttipos_P & Saídas/index.py
@@ -0,0 +1,7 @@
+
+# Desafio 3
+
+number_1 = float(input("1° número: "))
+number_2 = float(input("2° número: "))
+
+print("\n\n A soma dos números é " + str((number_1 + number_2)) + "\n\n")
