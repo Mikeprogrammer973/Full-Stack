ccdbd61933b1916a89ede8e9215ba40cd1fdeef4
Mike_Sognatore <mikepascal.delta@gmail.com>
2024-02-22 07:33:54 -0500

    instrução match __init__

--- Lab/Upload to Mega server/index.js
+++ Lab/Upload to Mega server/index.js
@@ -6,7 +6,6 @@ const storage = require('./storage')
 const url = require('url')
 
 const cors = require('cors');
-const { setTimeout } = require('timers/promises')
 app.use(cors({
     origin: '*'
 }))
@@ -87,5 +86,39 @@ app.get("/deleteFile", async (req, res)=>{
     await storage.delete_file(prms.file_name)
     res.end(prms.file_name)
 })
+
+
+
+// // Imports the Google Cloud client library
+// const {Translate} = require('@google-cloud/translate').v2;
+
+// // Creates a client
+// const translate = new Translate();
+// translate.key = "AIzaSyDGpdMFzdyJkSOP9YUSgpTH7apoJVQ2ZQA"
+// /**
+//  * TODO(developer): Uncomment the following lines before running the sample.
+//  */
+// // const text = 'The text to translate, e.g. Hello, world!';
+// // const target = 'The target language, e.g. ru';
+
+// async function translateText(text, target) {
+//   // Translates the text into the target language. "text" can be a string for
+//   // translating a single piece of text, or an array of strings for translating
+//   // multiple texts.
+//   let [translations] = await translate.translate(text, target);
+//   translations = Array.isArray(translations) ? translations : [translations];
+//   console.log('Translations:');
+//   translations.forEach((translation, i) => {
+//     console.log(`${text[i]} => (${target}) ${translation}`);
+//   });
+// }
+
+// translateText("I Love You!", 'tr');
+
+
+
+
+
+
 
 app.listen(3033, ()=> console.log("Server running..."))
--- Python/Flux/index.py
+++ Python/Flux/index.py
@@ -38,8 +38,30 @@ for number in range(2, 100):
         if number % x == 0:
             print('{} equals {} * {}'.format(number, x , number // x))
             break
-        else:
-            print('{} is a prime number'.format(number))
-            break
-        
+    else:
+        print('{} is a prime number'.format(number))
+       
 
+# comando continue
+
+for number in range(2, 20):
+    if number % 2 == 0:
+        print("Found an even number => {}".format(number))
+        continue
+    print("Found an odd number => {}".format(number))
+
+
+# instrução match
+
+def http_status_error(status):
+    match status:
+        case 400:
+            return "Bad request"
+        case 404: 
+            return "Not found"
+        case 345 | 897 | 345:
+            return "Not allowed"
+        case _:
+            return "There's something wrong"
+
+print(http_status_error(345), http_status_error(489), http_status_error(400), http_status_error(404))
