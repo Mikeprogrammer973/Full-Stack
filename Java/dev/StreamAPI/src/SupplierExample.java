e519c8d8eaaff514ff48c07819dab5812b626297
Mike_Sognatore <mikepascal.delta@gmail.com>
2024-06-28 17:17:08 -0400

    Supplier lambda function

--- Java/dev/StreamAPI/src/SupplierExample.java
+++ Java/dev/StreamAPI/src/SupplierExample.java
@@ -7,7 +7,9 @@ public class SupplierExample {
     public static void main(String[] args) {
         Supplier<String> saudacao = () -> "Hello, your are welcome!";
 
-        List<String> saudacoes = Stream.generate(saudacao).limit(7).collect(Collectors.toList());
+        //List<String> saudacoes = Stream.generate(saudacao).limit(7).collect(Collectors.toList());
+
+        List<String> saudacoes = Stream.generate(() -> "Hello, you are welcome!").limit(7).collect(Collectors.toList());
 
         saudacoes.forEach(s -> System.out.println(s));
     }
